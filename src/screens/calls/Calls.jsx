210dae4aefcb3c76b71772cd1bbb52e80a612cb3
jsusmachaca <falcorgd@gmail.com>
2024-06-26 16:48:20 -0500

    Init Calls

--- src/screens/calls/Calls.jsx
+++ src/screens/calls/Calls.jsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from "react"
 import { View, Text } from "react-native"
+import { useSQLiteContext } from 'expo-sqlite'
+
+const appId = '3842627e445d421b8908bea863efa541'
+const channelName = 'testChannel'
 
 export const Calls = () => {
+  const db = useSQLiteContext()
+
+  const [isAdmin, setIsAdmin] = useState(false)
+  const [engine, setEngine] = useState(null);
+  const [joined, setJoined] = useState(false);
+  const [remoteUid, setRemoteUid] = useState([]);
+
+
+  useEffect(() => {
+    const token = async () => {
+      try {
+        const [result] = await db.getAllAsync('SELECT * FROM user')
+        setIsAdmin(!!result.is_admin)
+      } catch (err) {
+        console.error(err.message)
+      }
+    }
+    token()
+  }, [db])
+
   return (
     <View style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <Text>Aún no disponible</Text>
