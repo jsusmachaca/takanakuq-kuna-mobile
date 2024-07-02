90461aa2f91d1e8a8f481c2206987547b9170802
jsusmachaca <falcorgd@gmail.com>
2024-07-02 01:45:52 -0500

    Update socketio implementation

--- src/screens/chat/Chat.jsx
+++ src/screens/chat/Chat.jsx
@@ -5,64 +5,73 @@ import { GiftedChat } from "react-native-gifted-chat"
 import { apiClient } from "../../utils/api/client"
 import io from 'socket.io-client'
 
-const socket = io('http://192.168.156.174:3000')
 
 export const Chat = () => {
   const db = useSQLiteContext()
-
   const [messages, setMessages] = useState([])
   const [userInfo, setUserInfo] = useState({})
-
-  const fetchingUserInfo = useCallback(async () => {
-    try {
+  const [socket, setSocket] = useState(null)
+  
+  useEffect(() => {
+    const fetchUserInfo = async () => {
       try {
         const [result] = await db.getAllAsync('SELECT * FROM user')
+        const token = result.token
+        
+        const newSocket = io('https://takanakuqkuna.com', {
+          extraHeaders: {
+            token: token
+          }
+        })
+
+        newSocket.on('connect', () => {
+          console.log('Connected to server')
+        })
+
+        newSocket.on('bot', (message) => {
+          const botMessage = {
+            _id: Math.random().toString(36).substring(7),
+            text: message,
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: 'Bot',
+            }
+          }
+          setMessages((previousMessages) => GiftedChat.append(previousMessages, botMessage))
+        })
+
+        setSocket(newSocket);
+
         const res = await apiClient.get('/api/user/data', {
           headers: {
-            Authorization: `Bearer ${result.token}`
+            Authorization: `Bearer ${token}`
           }
         })
         setUserInfo(res.data)
       } catch (err) {
         console.error(err.message)
       }
-    } catch (err) {
-      console.error(err.message)
     }
-  }, [userInfo, db])
 
-  useEffect(() => {
-    fetchingUserInfo()
-    
-    socket.on('connect', () => {
-      console.log('Connected to server')
-    })
+    fetchUserInfo()
 
-    socket.on('bot response', (message) => {
-      const botMessage = {
-        _id: Math.random().toString(36).substring(7),
-        text: message,
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'Bot',
-        }
-      }
-      setMessages((previousMessages) => GiftedChat.append(previousMessages, botMessage))
-    })
-    
     return () => {
-      socket.off('bot response')
-      socket.disconnect()
+      if (socket) {
+        socket.off('bot')
+        socket.disconnect()
+      }
     }
-  }, [])
+  }, [db])
 
   const onSend = useCallback((messages = []) => {
     setMessages(previousMessages =>
       GiftedChat.append(previousMessages, messages),
     )
-    socket.emit('chat message', messages[0].text)
-  }, [])
+    if (socket) {
+      socket.emit('chatbot', messages[0].text)
+    }
+  }, [socket])
 
   return (
     <ImageBackground source={require('./assets/bg.png')} resizeMode="cover" style={{ width: '100%', height: '100%' }}>
